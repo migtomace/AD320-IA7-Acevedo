8e2f86a66e8eada503140a131580ca011ca7e7aa
Miguel Acevedo <migtomace@gmail.com>
2019-03-05 04:20:10 -0800

    This fixes route2 issue

--- ad320-ia7-Acevedo/routes/route2.js
+++ ad320-ia7-Acevedo/routes/route2.js
@@ -2,21 +2,6 @@ const express = require('express'),
     router = express.Router(),
     colors = require('../colors.json');
 
-// router.get('/black', (req, res, next) => {
-//     // let object = colors.colors;
-//     // let colorName = req.params.color.toLowerCase();
-//     // for (let i; i < object.length; i++){
-//     //     console.log("Color JSON");
-//     //     if (JSON.stringify(object[i].color) === colorName){
-//     //         res.send(object[i].color);
-//     //     }
-//     // }
-//     // // if (object.color == colorName){
-//     // //     res.send(console.log("Color JSON"));
-//     // // }
-//     res.send(colors.colors);
-//     // res.status(404).send("Error 404: "+colorName+" Not found!");
-// });
 
 router.get('/all', (req, res, next) => {
     res.send(colors.colors);
@@ -26,7 +11,6 @@ router.get('/:color', (req, res, next) => {
     let object = colors.colors;
     let colorName = req.params.color.toLowerCase();
     for (let i in object){
-        console.log("Color JSON");
         if (object[i].color === colorName){
             res.send(object[i]);
         }
