6060efba06fbd8ec90d6940bff274bf9b83c8c21
Miguel Acevedo <migtomace@gmail.com>
2019-03-05 01:56:04 -0800

    This commits div color change on click

--- ad320-ia7-Acevedo/public/javascripts/main.js
+++ ad320-ia7-Acevedo/public/javascripts/main.js
@@ -1,12 +1,13 @@
 let div = document.getElementById("color");
+div.style.backgroundColor = "red";
 
 div.addEventListener("click", function () {
-    var color = div.css.background;
+    var color = div.style.backgroundColor;
 
     console.log("testing");
-    if(color === 'red'){
-        div.css.background = 'blue';
+    if(color === "red"){
+        div.style.backgroundColor = "blue";
     } else {
-        div.css.background = 'red';
+        div.style.backgroundColor = "red";
     }
 });
